07a11c71ded1c19b810be40eb2798509a365b56c
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-30 11:59:05 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
+
+setup(
+    name='whitehat',
+    version="0.1",
+    author='LyQuid',
+    author_email='lyquidpersonal@gmail.com',
+    description = 'Python Library For Ethical Hacker',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    keywords=["python", "hacker", "whitehat", "hacker tool", "ethical hacker"]
+)
