473b939373a4a0a40a8dc9bf4f959208f9eaa892
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-30 20:15:41 +0700

    Create my_wifi module
    Create the my_wifi module to see all the wifi passwords that have been connected to the device

---
+++ whitehat/my_wifi.py
@@ -0,0 +1,28 @@
+import subprocess
+import re
+
+class my_wifi:
+    def get_all_pw():
+        command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+        profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+        wifi_list = []
+        
+        if len(profile_names) != 0:
+            for name in profile_names:
+                wifi_profile = {}
+                profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+                if re.search("Security key           : Absent", profile_info):
+                    continue
+                else:
+                    wifi_profile["ssid"] = name
+                    profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+                    #    group after the : (which is the password).
+                    password = re.search("Key Content            : (.*)\r", profile_info_pass)
+                    if password == None:
+                        wifi_profile["password"] = None
+                    else:
+                        wifi_profile["password"] = password[1]
+                    wifi_list.append(wifi_profile) 
+        
+        for x in range(len(wifi_list)):
+            print(wifi_list[x])
