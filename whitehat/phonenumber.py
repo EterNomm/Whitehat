f0d62fe651eaf19b279b448c071f192a47e42fba
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-30 20:35:42 +0700

    Update phonenumber.py

--- whitehat/phonenumber.py
+++ whitehat/phonenumber.py
@@ -9,12 +9,16 @@ class phonenumber:
 
     def timezone(phone_number):
         tz = timezone.time_zones_for_number(the_number)
-        print(tz)
+        print(f"Timezone : {tz}")
 
     def carrier(phone_number):
         provider = carrier.name_for_number(the_number, "en")
-        print(provider)
+        print(f"Carrier : {provider}")
 
     def region(phone_number):
         region = geocoder.description_for_number(the_number, "en")
-        print(region)
+        print(f"Region : {region}")
+
+    def is_valid(phone_number):
+        is_valid = phonenumbers.is_valid_number(the_number)
+        print(f"Is Valid Phone Number : {is_valid}")
