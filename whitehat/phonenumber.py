cebed379198cc7fa4634a200eadc15bd2f7f6433
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-31 13:30:49 +0700

    Update phonenumber.py

--- whitehat/phonenumber.py
+++ whitehat/phonenumber.py
@@ -2,23 +2,40 @@ import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
 
 class phonenumber:
-    def set_number(phone_number):
-        global the_number
-        the_number = phonenumbers.parse(phone_number)
-        print(the_number)
+    def __init__(self, phone_number):
+        pn = phonenumbers.parse(phone_number)
+        self.phone_number = pn
+        print()
+        print(pn)
 
-    def timezone(phone_number):
-        tz = timezone.time_zones_for_number(the_number)
+    def timezone(self):
+        phone_number = self.phone_number
+        tz = timezone.time_zones_for_number(phone_number)
         print(f"Timezone : {tz}")
 
-    def carrier(phone_number):
-        provider = carrier.name_for_number(the_number, "en")
+    def carrier(self):
+        phone_number = self.phone_number
+        provider = carrier.name_for_number(phone_number, "en")
         print(f"Carrier : {provider}")
 
-    def region(phone_number):
-        region = geocoder.description_for_number(the_number, "en")
+    def region(self):
+        phone_number = self.phone_number
+        region = geocoder.description_for_number(phone_number, "en")
         print(f"Region : {region}")
 
-    def is_valid(phone_number):
-        is_valid = phonenumbers.is_valid_number(the_number)
+    def is_valid(self):
+        phone_number = self.phone_number
+        is_valid = phonenumbers.is_valid_number(phone_number)
         print(f"Is Valid Phone Number : {is_valid}")
+
+    def get_all_info(self):
+        phone_number = self.phone_number
+        tz = timezone.time_zones_for_number(phone_number)
+        provider = carrier.name_for_number(phone_number, "en")
+        region = geocoder.description_for_number(phone_number, "en")
+        is_valid = phonenumbers.is_valid_number(phone_number)
+        print(f"Timezone : {tz}")
+        print(f"Carrier : {provider}")
+        print(f"Region : {region}")
+        print(f"Is Valid Phone Number : {is_valid}")
+        print()
