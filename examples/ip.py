01a9c098f3b2c282bd257fcdc070dce69d29b882
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-10-31 19:42:30 +0700

    Update ip.py

--- examples/ip.py
+++ examples/ip.py
@@ -1,5 +1,9 @@
 import whitehat
 
 ip = whitehat.ip("ip address")
-ip.get_location()
-ip.get_info() # already including location
+
+ip_location = ip.get_location()
+ip_info = ip.get_info()  # already including location
+
+print(ip_location)
+print(ip_info)
