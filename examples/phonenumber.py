aa3b9c643173b39546874445844b7532cfe3425f
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-02 09:12:43 +0700

    phonenumber example updated

--- examples/phonenumber.py
+++ examples/phonenumber.py
@@ -1,11 +1,11 @@
 import whitehat
 
-pn = whitehat.phonenumber("phone_number") # Don't forget to use Country Code. Ex : +62 / +1
+pn = whitehat.phonenumber("+6281553888046") # Don't forget to use Country Code. Ex : +62 / +1
 
 print(pn.timezone)
 print(pn.carrier)
 print(pn.region)
-print(pn.is_valid)
+print(pn.is_valid())
 
 # to get all information
-print(pn.get_all_info())
+print(pn.get_all_info("list")) # Format : "json", "list"
