5c5c78a3601cf6673c06c11bec5e3b0f05e090ae
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-02 08:50:41 +0700

    Updated

--- examples/ip.py
+++ examples/ip.py
@@ -5,7 +5,7 @@ ip = whitehat.ip("ip address")
 
 web_ip = get_ip("example.com")
 ip_location = ip.location
-ip_info = ip.get_all_info()  # already including location
+ip_info = ip.get_all_info("format")  # already including location. Format : "json", "list"
 
 print(web_ip)
 print(ip_location)
