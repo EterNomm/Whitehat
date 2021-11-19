770be90259c99cfd25f863c728ff992784c2787e
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-11-19 13:21:09 +0700

    Update __init__.py

--- whitehat/__init__.py
+++ whitehat/__init__.py
@@ -5,3 +5,12 @@ from .ip import *
 from .keylogger import *
 from .port_scanner import *
 from .reverse_shell import *
+
+print("This Project fully owned by EterNomm")
+print("Join EterNomm Discord Here : https://dsc.gg/eternomm")
+
+__title__ = "Whitehat"
+__version__ = "1.0.0"
+__authors__ = ["LyQuid", "EterNomm"]
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021 EterNomm"
