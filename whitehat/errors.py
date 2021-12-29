c686df8f04447b2b8ea919e15b097db7cdb5fc28
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-29 19:47:22 +0700

    Update errors.py

--- whitehat/errors.py
+++ whitehat/errors.py
@@ -1,1 +1,5 @@
-# Soon !
+class WhitehatErrors(Exception):
+    def __init__(self, message:str=None):
+        if message is None:
+            message = 'An error has occurred within Whitehat'
+        super().__init__(message)
