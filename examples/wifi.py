296659d8d615ffb3319bde8fdc42d4827da43d94
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-03 13:13:10 +0700

    Update and rename my_wifi.py to wifi.py

--- examples/wifi.py
+++ examples/wifi.py
@@ -1,6 +1,5 @@
 import whitehat
 
-list_pw = whitehat.my_wifi.get_all_pw()
-print(list_pw)
+print(whitehat.Wifi.get_all_saved_pw())
 
 # This will print all the wifi names and passwords that have been connected to the device
