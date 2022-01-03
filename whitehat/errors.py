a3b07fcac843d83e8e6c0278bbf4ecd84999ad0c
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-03 14:39:22 +0700

    [Errors] Added lot of Errors

--- whitehat/errors.py
+++ whitehat/errors.py
@@ -7,3 +7,27 @@ class WhitehatErrors(Exception):
 class InvalidFormat(WhitehatErrors):
     def __init__(self):
         super().__init__("Invalid Format. Supported formats : 'list', 'json'")
+
+class DatabaseLocked(WhitehatErrors):
+    def __init__(self, browser:str="Chrome"):
+        super().__init__(f"Error : Database is locked. Make sure {browser} is closed!")
+
+class InvalidIP(WhitehatErrors):
+    def __init__(self):
+        super().__init__("Invalid IP. Cannot include: http:// or https://")
+
+class InvalidURL(WhitehatErrors):
+    def __init__(self):
+        super().__init__("Invalid URL. Cannot include: http:// or https://")
+
+class UnsupportedOS(WhitehatErrors):
+    def __init__(self, OS:str="Your OS", supported_os:str=None):
+        super().__init__(f"Error : {OS} is not supported on this function. Supported OS : {supported_os}")
+
+class CannotNone(WhitehatErrors):
+    def __init__(self, parameter: str = None):
+        super().__init__(f"Error : '{parameter}' cannot None")
+
+class UnsupportedBrowser(WhitehatErrors):
+    def __init__(self, Browser:str="Your Browser", supported_os:str=None):
+        super().__init__(f"Error : {Browser} is not supported on this function.")
