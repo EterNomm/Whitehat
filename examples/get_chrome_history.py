4ef408a191eb2382ed93491681cec297338255ff
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-05 16:33:17 +0700

    Update and rename get_all_chrome_history.py to get_chrome_history.py

--- examples/get_chrome_history.py
+++ examples/get_chrome_history.py
@@ -12,5 +12,5 @@ THIS METHOD IS STILL UNSTABLE, PLEASE DO NOT USE IT BEFORE RELEASE ON PYPI!
 
 import whitehat
 
-chrome = whitehat.Browser(browser_name="Chrome")
-print(chrome.get_all_history())
+chrome = whitehat.Browser("Chrome")
+chrome.get_history(show=100) # Default : all
