3831baa7162469005021716c1b288c32625203c2
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-06 16:45:46 +0700

    [Errors] Added more exceptions

--- whitehat/errors.py
+++ whitehat/errors.py
@@ -1,16 +1,16 @@
 class WhitehatErrors(Exception):
     def __init__(self, message:str=None):
         if message is None:
-            message = 'An error has occurred within Whitehat'
+            message = 'An unknown error has occurred within Whitehat.'
         super().__init__(message)
         
 class InvalidFormat(WhitehatErrors):
     def __init__(self):
-        super().__init__("Invalid Format. Supported formats : 'list', 'json'")
+        super().__init__("Invalid Format. Supported formats : 'list', 'json'.")
 
 class DatabaseLocked(WhitehatErrors):
-    def __init__(self, browser:str="Chrome"):
-        super().__init__(f"Error : Database is locked. Make sure {browser} is closed!")
+    def __init__(self, browser:str="your browser"):
+        super().__init__(f"Database is locked. Make sure {browser} is closed!")
 
 class InvalidIP(WhitehatErrors):
     def __init__(self):
@@ -22,12 +22,16 @@ class InvalidURL(WhitehatErrors):
 
 class UnsupportedOS(WhitehatErrors):
     def __init__(self, OS:str="Your OS", supported_os:str=None):
-        super().__init__(f"Error : {OS} is not supported on this function. Supported OS : {supported_os}")
+        super().__init__(f"{OS} is not supported on this function. Supported OS : {supported_os}.")
 
 class CannotNone(WhitehatErrors):
     def __init__(self, parameter:str=None):
-        super().__init__(f"Error : '{parameter}' cannot None")
+        super().__init__(f"'{parameter}' parameter cannot None.")
 
 class UnsupportedBrowser(WhitehatErrors):
     def __init__(self, Browser:str="Your Browser"):
-        super().__init__(f"Error : {Browser} is not supported on this function.")
+        super().__init__(f"{Browser} is not supported on this function.")
+
+class ShiftExceededTheLimit(WhitehatErrors):
+    def __init__(self, number:int=25):
+        super().__init__(f"Shift cannot more than {number}.")
