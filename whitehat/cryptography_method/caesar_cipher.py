c81927f9e4d9c6eba03e143b09af877d9e8b5b71
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-07 10:06:40 +0700

    [CaesarCipher] Change path to `cryptography_method`

---
+++ whitehat/cryptography_method/caesar_cipher.py
@@ -0,0 +1,49 @@
+import string
+from ..errors import *
+
+class CaesarCipher:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def encrypt(self, text, shift):
+        if text == None:
+            raise CannotNone("text")
+        if shift >= 26:
+            raise ShiftExceededTheLimit
+        
+        alphabets = [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]
+
+        def shift_alphabet(alphabets):
+            return alphabets[shift:] + alphabets[:shift]
+
+        shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+        final_alphabet = ''.join(alphabets)
+        final_shifted_alphabet = ''.join(shifted_alphabets)
+
+        table = str.maketrans(final_alphabet, final_shifted_alphabet)
+        
+        return text.translate(table)
+
+    @classmethod
+    def decrypt(self, cipher, shift):
+        if cipher == None:
+            raise CannotNone("cipher")
+        if shift >= 26:
+            raise ShiftExceededTheLimit
+
+        alphabets = [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]
+
+        shift = 26-shift
+
+        def shift_alphabet(alphabets):
+            return alphabets[shift:] + alphabets[:shift]
+
+        shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+        final_alphabet = ''.join(alphabets)
+        final_shifted_alphabet = ''.join(shifted_alphabets)
+
+        table = str.maketrans(final_alphabet, final_shifted_alphabet)
+
+        return cipher.translate(table)
