b20e2071ca8024aa18faacc5f4f43a1c2c408e5f
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-13 14:34:55 +0700

    [__init__] Removing `install_additional_packages`

--- whitehat/__init__.py
+++ whitehat/__init__.py
@@ -14,12 +14,3 @@ __version__ = "1.2.3"
 __authors__ = "LyQuid and EterNomm"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021-present EterNomm"
-
-
-import subprocess
-import os
-
-def install_additional_packages():
-    os.system("pip install pynput==1.7.4")
-    install = subprocess.check_output("pip install pynput==1.7.4", shell=True)
-    print(install.decode())
