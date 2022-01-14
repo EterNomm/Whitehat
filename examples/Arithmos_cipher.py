2fd383f306e85857603f0591524fd598cae6c6db
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-14 15:55:25 +0700

    Create Arithmos_cipher.py

---
+++ examples/Arithmos_cipher.py
@@ -0,0 +1,9 @@
+import whitehat
+
+# Encrypt
+encrypt = whitehat.Cryptography.ArithmosCipher.encrypt("Whitehat")
+print(encrypt)
+
+# Decrypt
+decrypt = whitehat.Cryptography.ArithmosCipher.decrypt(4908092005080120)
+print(decrypt)
