c9dad715538ef4e659cd4fa80c9fe1f101908c89
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-30 13:46:21 +0700

    Beta `1.4b0`

--- whitehat/__init__.py
+++ whitehat/__init__.py
@@ -11,7 +11,7 @@ from .cryptography import *
 from .images import PNG, JPEG
 
 __title__ = "Whitehat"
-__version__ = "1.3b"
+__version__ = "1.4b"
 __authors__ = "LyQuid and EterNomm"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2021-present EterNomm"
