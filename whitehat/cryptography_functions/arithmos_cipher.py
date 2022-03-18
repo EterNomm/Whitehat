80d1d90ea439cc53901a8e97edd379a3827e4547
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-18 17:34:25 +0700

    Using `arithmos-cipher` packages

--- whitehat/cryptography_functions/arithmos_cipher.py
+++ whitehat/cryptography_functions/arithmos_cipher.py
@@ -1,24 +1,22 @@
 # I made this cipher myself. If you want to see the repository, you can look at https://github.com/LyQuid12/arithmos-cipher
 
-from .dictionary import *
+import arithmos
 from ..errors import *
-import re
 
 class ArithmosCipher_function:
     def __init__(self):
         pass
 
     @classmethod
-    def encrypt(self, text:str=None):
+    def encrypt(self, text:str):
         if text == None:
             raise CannotNone("text")
-        return ''.join(encrypt_dict[c] for c in list(text))
+        else:
+            return arithmos.encrypt(text)
 
     @classmethod
-    def decrypt(self, cipher:int=None):
+    def decrypt(self, cipher:str):
         if cipher == None:
             raise CannotNone("cipher")
-        cipher = str(cipher)
-        split_num = re.findall("\d{2}|\w|.|\d\s|\s+|\d$", cipher)
-        decrypted = ''.join(decrypt_dict[c] for c in split_num)
-        return decrypted
+        else:
+            return arithmos.decrypt(cipher)
